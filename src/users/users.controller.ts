3c167b7f84f60e78829d7aa62e44ebf581ca4a2f
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-02 14:14:15 +0700

    done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,28 @@
-import { Controller, Get, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { LocalGuard } from './guards/local.guard';
 import { Request } from 'express';
 import { JwtGuard } from './guards/jwt.guard';
 
 @Controller('users')
 export class UsersController {
-    @Get('/status')
-    @UseGuards(JwtGuard)
-    status(@Req() req: Request) {
-        return req.user
-    }
+  @Get('/status')
+  @UseGuards(JwtGuard)
+  status(@Req() req: Request) {
+    return req.user;
+  }
 
-    @Post('/login')
-    @UseGuards(LocalGuard)
-    @UsePipes(new ValidationPipe())
-    token(@Req() req: Request) {
-        return req.user
-    }
+  @Post('/login')
+  @UseGuards(LocalGuard)
+  @UsePipes(new ValidationPipe())
+  token(@Req() req: Request) {
+    return req.user;
+  }
 }
